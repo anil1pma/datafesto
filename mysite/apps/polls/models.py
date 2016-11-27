14e5d7061d1a02654802089d6edf1e0b6ad4e7f9
anil.patel <anil.bhu2009@gmail.com>
2016-11-27 22:34:53 +0530

    changed db configuration

--- mysite/apps/polls/models.py
+++ mysite/apps/polls/models.py
@@ -1,7 +1,2 @@
 from django.db import models
 
-class User(models.Model):
-    user_name = models.CharField(max_length=50)
-    user_mob_no  = models.CharField(max_length=20)
-    user_mail_id = models.EmailField(max_length=50)
-    user_message = models.CharField(max_length=1000, blank=True)
